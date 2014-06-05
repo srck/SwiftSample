9037411dff9d7024b684704cfdb651d63b28920b
Sosuke Kimura <kimura.sousuke@members.co.jp>
2014-06-05 21:23:36 +0900

    initial commit.

--- SwiftSample/ViewController.swift
+++ SwiftSample/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "test"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
